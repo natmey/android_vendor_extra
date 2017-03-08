8078cc965366ec0ba3b76564b19c904bc570865b
Nat Meysenburg <nat@stealthisemail.com>
2017-03-08 16:17:15 -0500

    Trying to get this working.

--- product.mk
+++ product.mk
@@ -13,3 +13,11 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/CelloSuite.ogg:system/media/audio/alarms/CelloSuite.ogg \
 	$(LOCAL_PATH)/MasterOfPuppets.ogg:system/media/audio/notifications/MasterOfPuppets.ogg \
 	$(LOCAL_PATH)/AtTheGates.ogg:system/media/audio/ringtones/AtTheGates.ogg
+
+
+PRODUCT_PACKAGES += \
+	FDroid \
+	GmsCore \
+	FakeStore \
+  GfsProxy \
+  LocalGSMBackend \
