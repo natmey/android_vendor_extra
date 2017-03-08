cee972a46f30941860de9b3c0d2182ecdf0da1e7
Nat Meysenburg <nat@stealthisemail.com>
2017-03-07 23:08:31 -0500

    Initial commit

---
+++ product.mk
@@ -0,0 +1,15 @@
+CM_BUILDTYPE := CUSTOM
+
+DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/
+
+#
+# Include this file in a product makefile to include these audio files
+#
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/CelloSuite.ogg:system/media/audio/alarms/CelloSuite.ogg \
+	$(LOCAL_PATH)/MasterOfPuppets.ogg:system/media/audio/notifications/MasterOfPuppets.ogg \
+	$(LOCAL_PATH)/AtTheGates.ogg:system/media/audio/ringtones/AtTheGates.ogg
