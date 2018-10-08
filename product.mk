3083e058036e85a16151e24ed8f77a0976ff6467
Nat Meysenburg <nat@stealthisemail.com>
2018-10-08 17:10:35 -0400

    Add product.mk back in.

---
+++ product.mk
@@ -0,0 +1,16 @@
+CM_BUILDTYPE := CUSTOM
+
+DEVICE_PACKAGE_OVERLAYS += vendor/extra/overlay/
+
+#
+# Include this file in a product makefile to include these audio files
+#
+#
+
+LOCAL_PATH:= vendor/extra
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/CelloSuite.ogg:system/media/audio/alarms/CelloSuite.ogg \
+	$(LOCAL_PATH)/Bang-The-Drum-All-Day.ogg:system/media/audio/alarms/BangTheDrumAllDay.ogg \
+	$(LOCAL_PATH)/MasterOfPuppets.ogg:system/media/audio/notifications/MasterOfPuppets.ogg \
+	$(LOCAL_PATH)/AtTheGates.ogg:system/media/audio/ringtones/AtTheGates.ogg
